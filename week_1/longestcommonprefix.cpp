2149f6e6b139cda1e89016fd244d89648186085a
chandraprakash6719 <chandraprakashyadav6719@gmail.com>
2026-06-05 13:27:19 +0530

    Add files via upload

---
+++ week_1/longestcommonprefix.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs) {
+    string prefix = strs[0];
+
+    for (int i = 1; i < strs.size(); i++) {
+        while (strs[i].find(prefix) != 0) {
+            prefix.pop_back();
+
+            if (prefix.empty()) {
+                return "";
+            }
+        }
+    }
+
+    return prefix;
+}
+
+int main() {
+    vector<string> strs = {"flower", "flow", "flight"};
+
+    cout << "Longest Common Prefix: "
+         << longestCommonPrefix(strs);
+
+    return 0;
+}
